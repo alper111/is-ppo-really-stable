6559032e4b8e222af9921640d58c9accebb9a9b9
alper111 <ahmetoglu.alper@gmail.com>
2023-12-07 18:53:40 +0300

    change done to terminated and truncated

--- main_ppo.py
+++ main_ppo.py
@@ -6,7 +6,6 @@ import gym
 
 import models
 import utils
-
 env = gym.make(sys.argv[1])
 state_dim = torch.tensor(env.observation_space.shape).prod()
 action_dim = env.action_space.shape[0]
@@ -18,7 +17,7 @@ agent = models.PPOAgent(state_dim=state_dim, hidden_dim=64, action_dim=action_di
 it = 0
 update_iter = 4000
 max_timesteps = 1000
-show_freq = 10
+show_freq = 20
 gamma = 0.99
 lmbda = 0.97
 
@@ -35,17 +34,17 @@ for epi in range(750):
                 action = m.sample()
                 logprob, _ = agent.logprob(obs, action)
             states.append(obs.cpu())
-            obs, rew, done, _, _ = env.step(action.tolist())
+            obs, rew, terminated, truncated, _ = env.step(action.tolist())
             obs = torch.tensor(obs.reshape(-1), dtype=torch.float)
             actions.append(action.cpu())
             logprobs.append(logprob.cpu())
             rewards.append(rew)
-            if done:
+            if terminated or truncated:
                 break
 
         # add the last state for advantage estimation
         states.append(obs.cpu())
-        if not done:
+        if not terminated:
             with torch.no_grad():
                 rewards.append(agent.value(obs).item())
         else:
@@ -58,7 +57,7 @@ for epi in range(750):
         # gae
         with torch.no_grad():
             values = agent.value(states).reshape(-1)
-            if done:
+            if not terminated:
                 values[-1] = 0
             advantages = rewards[:-1] + gamma * values[1:] - values[:-1]
         discounted_adv = utils.discount(advantages, gamma * lmbda).cpu()
@@ -87,10 +86,10 @@ for epi in range(750):
             with torch.no_grad():
                 m = agent.dist(obs)
                 action = m.sample()
-            obs, _, done, _, _ = env_vis.step(action.tolist())
+            obs, _, terminated, _, _ = env_vis.step(action.tolist())
             obs = torch.tensor(obs.reshape(-1), dtype=torch.float)
             env.render()
-            if done:
+            if terminated:
                 break
         env_vis.close()
 
